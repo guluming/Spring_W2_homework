0c89f50ce609915a2c5182f5f5f50ed5c81fd5ce
guluming <guluming@naver.com>
2022-05-17 21:59:57 +0900

    추가&수정 Bus 클래스
    변경사항 : 1) 버스 고유번호 추가
                       2) enum 클래스 import화
                       3) busclose 메소드 추가
                           ㄴ버스 상태 '차고행'변경 메소드
                       4) setFuel 메소드 추가
                           ㄴ버스에 주유량 증가 메소드
                       5) takePassenger 메소드 변경
                           ㄴ승객 합산 메커니즘 변경 & '탑승 가능 승객'표시 및 '탑승 못한 승객'표시

--- busSystem/src/main/java/Bus.java
+++ busSystem/src/main/java/Bus.java
@@ -1,75 +1,83 @@
+import BusStatePkg.StateType;
+
 class Bus {
+    static int count = 0;
+    int serialNum;
+    {
+        count++;
+        serialNum = count;
+    }
     int busNum;
     StateType state;
     int passenger;
+    int nowPassenger;
     int maxPassenger;
     int fuel;
     int speed;
     int fare;
 
-    public Bus(int busNum, StateType state, int passenger, int maxPassenger, int fuel, int speed, int fare) {
+    public Bus(int busNum, StateType state, int passenger, int nowPassenger, int maxPassenger, int fuel, int speed, int fare) {
         this.busNum = busNum;
         this.state = state;
         this.passenger = passenger;
+        this.nowPassenger = nowPassenger;
         this.maxPassenger = maxPassenger;
         this.fuel = fuel;
         this.speed = speed;
         this.fare = fare;
     }
 
-    enum StateType{
-        R("RACE"), P("PARKING");
-        final private String name;
-        public String getName() {
-            return name;
-            }
-        private StateType(String name){
-            this.name = name;
-            }
-    }
-
     public Bus(int busNum) {
-        this(busNum, StateType.R, 0, 10, 0, 0, 500);
+        this(busNum, StateType.R, 0, 0, 10, 0, 0, 500);
     }
 
-    public void setState(int fuel, StateType state){
-        this.state = StateType.R;
-        if(!state.equals(StateType.R)){
-            this.state = StateType.P;
-        }
+    public void busClose(){
+        this.state = StateType.P;
         if(fuel<10){
-            this.state = StateType.P;
-            System.out.println("Bus need fuel");
+            System.out.println("주유가 필요하다");
         }
-        this.fuel = fuel;
     }
 
-    public void setSpeed(int fuel, int speed){
+    public void setFuel(int fuel){
+        this.fuel += fuel;
+        if(this.fuel<10){
+            this.state = StateType.P;
+            System.out.println("주유가 필요하다");
+        } else {
+            this.state = StateType.R;
+        }
+    }
+
+    public void setSpeed(int speed){
         if(fuel>=10){
             if(speed>=0){
                 this.speed += speed;
-                System.out.println("speed up");
+                System.out.println("속도 증가");
             } else {
                 this.speed += speed;
-                System.out.println("speed down");
+                System.out.println("속도 감소");
             }
         } else {
-            System.out.println("plz cheak fuel");
+            this.state = StateType.P;
+            System.out.println("주유량을 확인해 주세요");
         }
-        this.fuel = fuel;
     }
 
-   public void takePassenger(StateType state){
+   public void takePassenger(int passenger){
         if(state.equals(StateType.R)){
-            this.state = state;
-            this.passenger++;
-            if(passenger>maxPassenger){
-                this.passenger--;
-                System.out.println("passenger is full");}
-        } else {
-            this.state = StateType.P;
-            this.passenger = 0;
+            this.nowPassenger = nowPassenger + passenger;
+            if((nowPassenger)>maxPassenger){
+                System.out.println("승객이 최대치 입니다");
+            }
+                if((maxPassenger-nowPassenger)>0){
+                    System.out.println("탑승 가능한 승객 " + (maxPassenger-nowPassenger));
+                }else{
+                    System.out.println("탑승 가능한 승객 " + 0 );
+                    System.out.println("탑승 못한 승객 " + Math.abs(maxPassenger-nowPassenger));
+                    this.nowPassenger = maxPassenger;
+                 }
+        }else{
             System.out.println(state.getName());
-        }
+         }
    }
 }
