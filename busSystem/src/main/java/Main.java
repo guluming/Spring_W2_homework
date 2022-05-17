c24bae2aa2fa5719a8be61dac7e504d835f6e301
guluming <guluming@naver.com>
2022-05-17 14:46:11 +0900

    enum 사용 테스트

--- busSystem/src/main/java/Main.java
+++ busSystem/src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Bus bus1 = new Bus(6);
-        bus1.takePassenger("race");
-        bus1.setSpeed(11, 5);
+//        bus1.takePassenger(Bus.StateType.R);
+        bus1.setSpeed(1, 5);
         bus1.setSpeed(11, 5);
         bus1.setSpeed(11, -2);
-        bus1.setState(11, "race");
-        bus1.setState(9, "race");
+//        bus1.setState(11, Bus.StateType.R);
+//        bus1.setState(19, Bus.StateType.P);
 
 
-        System.out.println(bus1.busNum +"/"+bus1.state+"/"+ bus1.passenger +"/"+ bus1.maxPassenger +"/"+ bus1.Fuel +"/"+ bus1.speed +"/"+ bus1.fare);
+        System.out.println(bus1.busNum +"/"+bus1.state+"/"+ bus1.passenger +"/"+ bus1.maxPassenger +"/"+ bus1.fuel +"/"+ bus1.speed +"/"+ bus1.fare);
     }
 }
