e47586f1587af1c28a94443dd9272c408d12a195
guluming <guluming@naver.com>
2022-05-17 21:46:12 +0900

    enum 클래스 import 화

--- busSystem/src/main/java/BusStatePkg/StateType.java
+++ busSystem/src/main/java/BusStatePkg/StateType.java
@@ -1,2 +1,12 @@
-package BusStatePkg;public class StateType {
+package BusStatePkg;
+
+public enum StateType {
+    R("운행"), P("차고지행");
+    final private String name;
+    private StateType(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
